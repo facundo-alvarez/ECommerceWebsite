52683c24c4fd2bdc4db1f75a50849ab8da82c7c7
facundo-alvarez <alvarez-facundo@hotmail.com>
2022-05-30 14:06:42 +0200

    Added quantity ajax selector in cart

--- src/Web/Pages/Cart/Index.cshtml.cs
+++ src/Web/Pages/Cart/Index.cshtml.cs
@@ -34,6 +34,45 @@ namespace Web.Pages.Cart
             }
         }
 
+        public void OnGetRemoveQuantity(int id, int quantity)
+        {
+            Items = new List<Item>();
+            int existingQuantity = 0;
+
+            if (HttpContext.Session.Get<List<Item>>(SiteConstants.SessionCart) != null && HttpContext.Session.Get<List<Item>>(SiteConstants.SessionCart).Count() > 0)
+            {
+                Items = HttpContext.Session.Get<List<Item>>(SiteConstants.SessionCart);
+                existingQuantity = Items.FirstOrDefault(i => i.Product.Id == id).Quantity;
+
+                if(existingQuantity != 1)
+                {
+                    existingQuantity -= 1;
+                    Items.FirstOrDefault(i => i.Product.Id == id).Quantity = existingQuantity;
+                    HttpContext.Session.Set<List<Item>>(SiteConstants.SessionCart, Items);
+                }
+            }
+        }
+
+        public void OnGetAddQuantity(int id, int quantity)
+        {
+            Items = new List<Item>();
+            int existingQuantity = 0;
+
+            if (HttpContext.Session.Get<List<Item>>(SiteConstants.SessionCart) != null && HttpContext.Session.Get<List<Item>>(SiteConstants.SessionCart).Count() > 0)
+            {
+                Items = HttpContext.Session.Get<List<Item>>(SiteConstants.SessionCart);
+                existingQuantity = Items.FirstOrDefault(i => i.Product.Id == id).Quantity;
+
+                if (existingQuantity != 20)
+                {
+                    existingQuantity += 1;
+                    Items.FirstOrDefault(i => i.Product.Id == id).Quantity = existingQuantity;
+                    HttpContext.Session.Set<List<Item>>(SiteConstants.SessionCart, Items);
+                }
+            }
+        }
+
+
         public PartialViewResult OnGetPartialCart()
         {
             Items = new List<Item>();
@@ -57,5 +96,6 @@ namespace Web.Pages.Cart
 
             return Partial("_CartListPartial", Items);
         }
+
     }
 }
